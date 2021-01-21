7c4ab6c94c6c70a2efa7b54abf7548a83c592e55
Val-erieS <72756972+Val-erieS@users.noreply.github.com>
2021-01-21 12:17:56 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 while True:
-    print("sound level" + input.sound_level()) and print("light level:" + input.light_level())
-    if input.sound_level() > 100:
-        light.show_animation(light.rainbowAnimation, 500)
-    if input.light_level() > 15:
-        music.magic_wand.play_until_done()
-else: 
-    music.stop_all_sounds() and light.set_all(light.rgb(0,0,0))
+    if input.sound_level() >= 150 and input.light_level() > 15: 
+        light.show_animation(light.rainbowAnimation, 500) and music.magic_wand.play_until_done()
+    else: 
+        music.stop_all_sounds() and light.set_all(light.rgb(0,0,0))
--- main.ts
+++ main.ts
@@ -1,1 +1,8 @@
-
+while (true) {
+    if (input.soundLevel() >= 150 && input.lightLevel() > 15) {
+        light.showAnimation(light.rainbowAnimation, 500) && music.magicWand.playUntilDone()
+    } else {
+        music.stopAllSounds() && light.setAll(light.rgb(0, 0, 0))
+    }
+    
+}
